48915472286a76d47664216cefa7f7cfecc0df49
MayankSonii <95433553+MayankSonii@users.noreply.github.com>
2023-05-13 20:15:37 +0530

    Add files via upload

---
+++ success.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width = device-width, initial-scale = 1">
+        <title>Success </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php include 'includes/header.php'; ?>
+        <div class="container-fluid" id="content">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                      <h3 align="center">Your order is confirmed. Thank you for shopping with us.</h3><hr>
+                    <p align="center">Click <a href="products.php">here</a> to purchase any other item.</p>
+                </div>
+            </div>
+        </div>
+        <?php include("includes/footer.php");
+        ?>
+    </body>
+</html>
